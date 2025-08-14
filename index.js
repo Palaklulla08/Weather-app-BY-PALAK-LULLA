3ff69db94e8aa42479a1091351bd59c61d18daf2
Palaklulla08 <palaklulla54@gmail.com>
2025-08-14 16:27:21 +0530

    function for 5days forecast

--- index.js
+++ index.js
@@ -51,3 +51,33 @@ async function checkWeather(city) {
     }}
     notification(warn)
 }
+// FUNCTION FOR 5 DAYS WEATHER FORECASTS
+async function daysForecast(city) {
+    const response1 = await fetch(apiUrl1 + `&q=${city}` + `&appid=${apiKey}`);
+    var data1 = await response1.json();
+
+    console.log(data1);
+    document.querySelector('.date1').innerHTML = data1.list[2].dt_txt;
+    document.querySelector('.temperature1').innerHTML = Math.round(data1.list[2].main.temp) + "°C";
+    document.querySelector('.humid1').innerHTML = data1.list[2].main.humidity + "%";
+    document.querySelector('.windair1').innerHTML = data1.list[2].wind.speed + "m/s";
+
+    document.querySelector('.date2').innerHTML = data1.list[10].dt_txt;
+    document.querySelector('.temperature2').innerHTML = Math.round(data1.list[10].main.temp) + "°C";
+    document.querySelector('.humid2').innerHTML = data1.list[10].main.humidity + "%";
+    document.querySelector('.windair2').innerHTML = data1.list[10].wind.speed + "m/s";
+
+    document.querySelector('.date3').innerHTML = data1.list[18].dt_txt;
+    document.querySelector('.temperature3').innerHTML = Math.round(data1.list[18].main.temp) + "°C";
+    document.querySelector('.humid3').innerHTML = data1.list[18].main.humidity + "%";
+    document.querySelector('.windair3').innerHTML = data1.list[18].wind.speed + "m/s";
+
+    document.querySelector('.date4').innerHTML = data1.list[26].dt_txt;
+    document.querySelector('.temperature4').innerHTML = Math.round(data1.list[26].main.temp) + "°C";
+    document.querySelector('.humid4').innerHTML = data1.list[26].main.humidity + "%";
+    document.querySelector('.windair4').innerHTML = data1.list[26].wind.speed + "m/s";
+
+    document.querySelector('.date5').innerHTML = data1.list[34].dt_txt;
+    document.querySelector('.temperature5').innerHTML = Math.round(data1.list[34].main.temp) + "°C";
+    document.querySelector('.humid5').innerHTML = data1.list[34].main.humidity + "%";
+    document.querySelector('.windair5').innerHTML = data1.list[34].wind.speed + "m/s";
