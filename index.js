b227865cd00790af8caa9a2a65544d59f9eb1171
Palaklulla08 <palaklulla54@gmail.com>
2025-08-14 16:22:14 +0530

    constant created

---
+++ index.js
@@ -0,0 +1,12 @@
+const apiKey = `6fbfaf34a6b217687723315505218c74`;
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric'
+const apiUrl1 = 'https://api.openweathermap.org/data/2.5/forecast?units=metric'
+
+const searchBox = document.querySelector('.search-input')
+const searchBtn = document.querySelector('.search-btn')
+const weatherIcon = document.querySelector('.weather-icon')
+const forcastIcon1 =document.querySelector('.forcasticon-1')
+const forcastIcon2 =document.querySelector('.forcasticon-2')
+const forcastIcon3 =document.querySelector('.forcasticon-3')
+const forcastIcon4 =document.querySelector('.forcasticon-4')
+const forcastIcon5 =document.querySelector('.forcasticon-5')
