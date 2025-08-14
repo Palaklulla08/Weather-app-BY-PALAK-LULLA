b4edf373276a5a4222330a12527cdcf07a83d63b
Palaklulla08 <palaklulla54@gmail.com>
2025-08-14 16:22:57 +0530

    function for city data fetching

--- index.js
+++ index.js
@@ -10,3 +10,36 @@ const forcastIcon2 =document.querySelector('.forcasticon-2')
 const forcastIcon3 =document.querySelector('.forcasticon-3')
 const forcastIcon4 =document.querySelector('.forcasticon-4')
 const forcastIcon5 =document.querySelector('.forcasticon-5')
+
+// FUNCTION FOR DATA FETCHING BY CITY
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`);
+    var  data = await response.json();
+
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + " M/S";
+
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "/clear.png"
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "/mist.png";
+    }
+    else if (data.weather[0].main == "Snow") {
+        weatherIcon.src = "/snow.png";
+    }
+   
+  
